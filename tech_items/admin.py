0ec86d14c32ec49f8ae146d2cadc27151d650d22
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2024-10-21 18:16:14 +0300

    Add filtering by out and in item types in RelationAdminForm

--- tech_items/admin.py
+++ tech_items/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django import forms
 
-# Register your models here.
+
+from tech_items.models import Relation, Item, RelationType, Figure, ItemType, LineEnd
+
+
+class RelationAdminForm(forms.ModelForm):
+    class Meta:
+        model = Relation
+        fields = '__all__'
+
+    class Media:
+        js = (
+            'admin/js/vendor/jquery/jquery.js',  # Django's built-in jQuery
+            'js/relation-admin.js',
+        )
+
+
+class RelationAdmin(admin.ModelAdmin):
+    form = RelationAdminForm
+
+
+admin.site.register(Relation, RelationAdmin)
+
+# admin.site.register(Relation)
+admin.site.register(Item)
+admin.site.register(ItemType)
+admin.site.register(RelationType)
+admin.site.register(Figure)
+admin.site.register(LineEnd)
--- tech_items/views.py
+++ tech_items/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views import View
+from .models import Item, RelationType
 
-# Create your views here.
+def index(request):
+    return render(request, 'tech_items/index.html',
+                  )
+
+
+class FilterOutItemView(View):
+    def get(self, request):
+        relation_type_id = request.GET.get('relation_type')
+        data = []
+        if relation_type_id:
+            try:
+                relation_type = RelationType.objects.get(pk=relation_type_id)
+                items = Item.objects.filter(type=relation_type.out_type)
+                data = [{'id': item.id, 'name': item.name} for item in items]
+            except RelationType.DoesNotExist:
+                pass
+        return JsonResponse({'items': data})
+
+
+class FilterInItemView(View):
+    def get(self, request):
+        relation_type_id = request.GET.get('relation_type')
+        data = []
+        if relation_type_id:
+            try:
+                relation_type = RelationType.objects.get(pk=relation_type_id)
+                # Assuming 'in_item_type' is the field that references ItemType for in items
+                items = Item.objects.filter(type=relation_type.in_type)
+                data = [{'id': item.id, 'name': item.name} for item in items]
+            except RelationType.DoesNotExist:
+                pass
+        return JsonResponse({'items': data})
